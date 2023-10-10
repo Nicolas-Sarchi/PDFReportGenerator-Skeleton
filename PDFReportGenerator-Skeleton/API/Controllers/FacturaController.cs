7b50ee6eba21c2856093821adaa7b3f838c32c8a
KevinRinc0n <rinconkdavid@gmail.com>
2023-10-10 15:42:31 -0500

    feat: :sparkles: sparkles: try-catch added

--- PDFReportGenerator-Skeleton/API/Controllers/FacturaController.cs
+++ PDFReportGenerator-Skeleton/API/Controllers/FacturaController.cs
@@ -40,28 +40,35 @@ namespace API.Controllers
     }
 
 
-[HttpPost]
-[ProducesResponseType(StatusCodes.Status201Created)]
-[ProducesResponseType(StatusCodes.Status400BadRequest)]
-public async Task<ActionResult<FacturaDto>> Post(FacturaPostDto factDto)
-{
-    var factura = this.mapper.Map<Factura>(factDto);
-
-    _unitOfWork.Facturas.Add(factura);
-    await _unitOfWork.SaveAsync();
-
-    var detallesFactura = new List<DetalleFactura>();
-
-    foreach (var detalleFacturaDto in factDto.DetallesFactura)
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<FacturaDto>> Post(FacturaPostDto factDto)
     {
-        var detalleFactura = this.mapper.Map<DetalleFactura>(detalleFacturaDto);
-        detalleFactura.IdFacturaFk = factura.Id;
-        detallesFactura.Add(detalleFactura);
+        try
+        {
+            var factura = this.mapper.Map<Factura>(factDto);
+
+        _unitOfWork.Facturas.Add(factura);
+        await _unitOfWork.SaveAsync();
+
+        var detallesFactura = new List<DetalleFactura>();
+
+        foreach (var detalleFacturaDto in factDto.DetallesFactura)
+        {
+            var detalleFactura = this.mapper.Map<DetalleFactura>(detalleFacturaDto);
+            detalleFactura.IdFacturaFk = factura.Id;
+            detallesFactura.Add(detalleFactura);
+        }
+        factura = await _unitOfWork.Facturas.GetByIdAsync(factura.Id);
+        var facturaDto = this.mapper.Map<FacturaDto>(factura);
+
+        return Ok(facturaDto);
+        }catch(Exception ex)
+        {
+            return BadRequest($"Hubo un error al generar el pdf :{ex.Message}");
+        }
     }
-    factura = await _unitOfWork.Facturas.GetByIdAsync(factura.Id);
-    var facturaDto = this.mapper.Map<FacturaDto>(factura);
 
-      return Ok(facturaDto);
+    }
 }
-
-}}
